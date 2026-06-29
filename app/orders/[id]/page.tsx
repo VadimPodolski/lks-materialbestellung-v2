ec3569a2edefdb777c1fab172ab3c9d1d37a8f57
VadimPodolski <bg6d52y8r8@privaterelay.appleid.com>
2026-06-29 19:25:11 +0200

    Update page.tsx

--- app/orders/[id]/page.tsx
+++ app/orders/[id]/page.tsx
@@ -336,10 +336,9 @@ LKS-Technik GmbH & Co. KG`
       `Zum Löschen bitte die Auftragsnummer eingeben:\n${order.order_number}`
     )
 
-    if (check !== order.order_number) {
-      alert('Auftragsnummer stimmt nicht überein.')
-      return
-    }
+    if (!confirm(`Bestellung ${order.order_number} wirklich löschen?`)) {
+  return
+}
 
     const supabase = createClient()
 
