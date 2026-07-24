34d9b58308b557b79e031403578057d32dc5b921
VadimPodolski <VadimPodolski@users.noreply.github.com>
2026-07-24 09:27:23 +0200

    Include PDFKit fonts in mail function

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['pdfkit'],
+    outputFileTracingIncludes: {
+      '/api/send-order-mail': ['./node_modules/pdfkit/js/data/*.afm']
+    }
+  }
+};
 export default nextConfig;
